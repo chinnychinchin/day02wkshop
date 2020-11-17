57f75de50884bf17e2cddb34365c62d1958c93ed
chinyanlim <chinyan77@gmail.com>
2020-11-17 19:14:31 +0800

    to be continued

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InventoryComponent } from './components/inventory.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventoryComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/inventory.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory',
+  templateUrl: './inventory.component.html',
+  styleUrls: ['./inventory.component.css']
+})
+export class InventoryComponent implements OnInit {
+
+  inventory = [{item: 'Apples', imgUrl: "assets/apple.png"},{item: 'Oranges', imgUrl: "assets/orange.png"},{item: 'Pears', imgUrl: "assets/pear.png"}]
+  appleCount:number = 0;
+  orangeCount:number = 0;
+  pearCount:number = 0;
+
+  constructor() { }
+
+  onAdd($event) {
+    console.log($event.target.value)
+    switch($event.target.value) {
+      case 'Apples': 
+        this.appleCount += 1;
+        break;
+      case 'Oranges': 
+        this.orangeCount += 1
+        break;
+      case 'Pears': 
+        this.pearCount += 1;
+        break;
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+}
